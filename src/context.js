3d55b4a75f57740f0c1d81069671fddf91b9cb68
gnoul_ <ttluong4869@gmail.com>
2021-10-22 18:50:17 +0700

    Complete hero and sidebar

--- src/context.js
+++ src/context.js
@@ -1,2 +1,46 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    const [isSidebarShown, setIsSidebarShown] = useState(false);
+    const [isSubmenuShown, setIsSubmenuShown] = useState(true);
+
+    const showSidebar = () => {
+        setIsSidebarShown(true);
+    };
+
+    const hideSidebar = () => {
+        setIsSidebarShown(false);
+    };
+
+    const showSubmenu = () => {
+        setIsSubmenuShown(true);
+    };
+
+    const hideSubmenu = () => {
+        setIsSubmenuShown(false);
+    };
+
+    return (
+        <AppContext.Provider
+            value={{
+                isSidebarShown,
+                showSidebar,
+                hideSidebar,
+                isSubmenuShown,
+                showSubmenu,
+                hideSubmenu,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+const useGlobalContext = () => {
+    return useContext(AppContext);
+};
+
+export { AppProvider, useGlobalContext };
